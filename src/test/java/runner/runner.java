3ee259a3d38d86f3ffb63a5b89a647433800ed02
nazik7 <59424131+nazik7@users.noreply.github.com>
2022-11-16 13:28:02 -0600

    jira rest api

--- src/test/java/runner/runner.java
+++ src/test/java/runner/runner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/CreatingSprint.feature",
+        features = "src/test/resources/",
         glue = "StepDefinitions",
         dryRun = false,
         monochrome = true
